2daed7c9a91589e8d6dd4e2b292e8890930060b4
agustindiazantuna <agustin.diazantuna@gmail.com>
2019-11-06 00:24:06 -0300

    Changes on module.py
    Open serial port code

--- python/Mercurial_SDR.py
+++ python/Mercurial_SDR.py
@@ -36,8 +36,8 @@ class Mercurial_SDR(gr.sync_block):
             in_sig=[numpy.float32],
             out_sig=[numpy.float32])
 
-        subprocess.call('icepll')
-        subprocess.call('echo "HOLA\n"', shell=True)
+        # subprocess.call('icepll')
+        # subprocess.call('echo "HOLA\n"', shell=True)
 
         #ser = serial.Serial('/dev/pts/13')  # open serial port
         #print(ser.name)         # check which port was really used
@@ -45,18 +45,24 @@ class Mercurial_SDR(gr.sync_block):
         #ser.close()             # close port
         #ser.baudrate = 9600
         #print(ser.baudrate)
-        subprocess.call(['echo', 'sin invocar shell\n'])
+        # subprocess.call(['echo', 'sin invocar shell\n'])
         #self.modulation = modulation_key
         #print(self.modulation)
         #print("MOD")
         var ='hola'
-        value = 10  
-        f= open("defines.v","w+")
-        for i in range(10):
-            f.write("`define %d \r\n" % bits_key)
-        f.close()   
+        value = 10
 
+        # open file and write header
+        f = open("/home/agustin/Dropbox/0_2019/PF_2019/sdr-tx/hdl/inc/module_params.v","w+")
+        f.write("`ifndef __PROJECT_CONFIG_V\n`define __PROJECT_CONFIG_V\n\n")
+        f.write("`define AM_CLKS_PER_PWM_STEP %d\n" % 1)
+        f.write("`define AM_PWM_STEP_PER_SAMPLE %d\n" % 63)
+        f.write("`define AM_BITS_PER_SAMPLE %d\n" % bits_key)
+        f.write("\n`endif")
+        f.close()
 
+        subprocess.call('/home/agustin/Dropbox/0_2019/PF_2019/sdr-tx/hdl/syn/run_in_docker.sh make')
+        subprocess.call('/home/agustin/Dropbox/0_2019/PF_2019/sdr-tx/hdl/syn/run_in_docker.sh make prog')
 
 
 
@@ -66,13 +72,11 @@ class Mercurial_SDR(gr.sync_block):
         # <+signal processing here+>
         out[:] = in0
 
-
         return len(output_items[0])
-
-
 
     def set_modulation(self,modulation_key):
         self.modulation = modulation_key
         print("modulationaaaa")
         print(self.modulation)
+
         
